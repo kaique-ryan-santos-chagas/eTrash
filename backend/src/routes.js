a377fce32fe571db66e6cd91dbbc0788133ca528
Matias Ricardo Muente Castro <matiascastro@outlook.com.br>
2020-04-18 20:13:57 -0300

    routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,12 +5,17 @@ const CompaniesController = require('./controllers/CompaniesController');
 const UserController = require('./controllers/UserController');
 const PointController = require('./controllers/PointController');
 const SessionController = require('./controllers/SessionController');
-
+const SessionCompaniesController = require('./controllers/SessionCompaniesController')
 
 routes.get('/users', UserController.index);
 routes.post('/users/create', UserController.create);
 routes.delete('/users/delete', UserController.delete);
-routes.post('/session', SessionController.create);
 
+routes.post('/session', SessionController.create);
+routes.post('/session/companies', SessionController.create);
+
+routes.post('/companies/create',CompaniesController.create);
+routes.get('/companies',CompaniesController.index);
+routes.delete('/companies/delete',CompaniesController.delete);
 
 module.exports = routes;
