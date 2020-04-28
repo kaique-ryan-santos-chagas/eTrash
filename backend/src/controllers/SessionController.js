ccb08d0f160a8c80477a2774b963c44e7ae1aee8
Kaique Ryan <kaique.github@gmail.com>
2020-04-28 17:33:36 -0300

    Update Session for session company and dsiacart points

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const axios = require('axios');
 
 module.exports = {
-	create: async (req, res) => {
+	userCreate: async (req, res) => {
 		const {username, passwordInput} = req.body;
 		const usernameDB = await connection('users').where('name', username)
 		.select('name').first();
@@ -26,6 +26,56 @@ module.exports = {
 		const localLon = dataCoord.data.lon;
 		await connection('users').update({latitude: localLat, longitude: localLon });
 		return res.json({user: username});
+	},
+
+	companyCreate: async (req, res) => {
+		const {name, passwordInput} = req.body;
+        const companieNameDB = await connection('companies').where('name', name)
+        .first();
+
+        if (!companieNameDB) {
+            return res.status(400).json({error: 'Empresa não encontrada'});
+        }
+
+        const companiePasswordDB = await connection('companies').where('name', name)
+        .select('password')
+        .first();
+        const passwordMatch = await bcrypt.compareSync(passwordInput, companiePasswordDB.password);
+        
+        if (!passwordMatch) {
+            return res.status(400).json({error: 'Senha Inválida'});
+        }
+
+        const dataCoord = await axios.get('http://www.ip-api.com/json');
+        const localLat = dataCoord.data.lat;
+        const localLon = dataCoord.data.lon;
+        await connection('companies').update({latitude: localLat, longitude: localLon });
+        return res.json({companie: name});
+	},
+
+	pointCreate: async (req, res) => {
+		const {name, passwordInput} = req.body;
+        const pointNameDB = await connection('discarts_points').where('name', name)
+        .first();
+
+        if (!pointNameDB) {
+            return res.status(400).json({error: 'Empresa não encontrada'});
+        }
+
+        const pointPasswordDB = await connection('discarts_points').where('name', name)
+        .select('password')
+        .first();
+        const passwordMatch = await bcrypt.compareSync(passwordInput, pointPasswordDB.password);
+        
+        if (!passwordMatch) {
+            return res.status(400).json({error: 'Senha Inválida'});
+        }
+
+        const dataCoord = await axios.get('http://www.ip-api.com/json');
+        const localLat = dataCoord.data.lat;
+        const localLon = dataCoord.data.lon;
+        await connection('discarts_points').update({latitude: localLat, longitude: localLon });
+        return res.json({point: name});
 	}
 
 };
