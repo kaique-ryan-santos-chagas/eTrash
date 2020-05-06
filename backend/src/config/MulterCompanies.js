c764846de112535b7af4a11ba9b2b6a5f7426a43
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2020-05-06 14:04:35 -0300

    Multer of Companies

---
+++ backend/src/config/MulterCompanies.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
+
+module.exports = {
+
+    dest: path.resolve(__dirname,'..','..','temp','uploads','companies'),
+    storage: multer.diskStorage({
+        destination: (req,file,cb) =>{
+            cb(null, path.resolve(__dirname,'..','..','temp','uploads','companies'));
+        },
+        filename: (req,file,cb) =>{
+            crypto.randomBytes(16,(err, hash)=> {
+                if(err) cb(err);
+
+                const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+                cb(null,fileName);
+            });
+        }
+    }),
+    limits:{
+    filesize: 2*1024*1024,
+    },
+    fileFilter: (req, file, cb) =>{
+        const allowedMimmes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png',
+            'image/gif'
+        ];
+
+        if(allowedMimmes.includes(file.mimetype)){
+            cb(null, true);
+        }else{
+            cb(new Error('Extensão de arquivo inválida.'));
+        }
+
+    },
+};
