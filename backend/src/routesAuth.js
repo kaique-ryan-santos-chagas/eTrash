919e5e0efc7011691b7c1c37ed3e4980a48c8e7c
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2020-05-06 14:02:49 -0300

    Routes with authentication

--- backend/src/routesAuth.js
+++ backend/src/routesAuth.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
 const routesAuth = express.Router();
 
 const CompaniesController = require('./controllers/CompaniesController');
 const UserController = require('./controllers/UserController');
 const PointController = require('./controllers/PointController');
 const DiscartController = require('./controllers/DiscartController');
+
 const authMiddleware = require('./middlewares/auth');
+const MulterUsers = require('./config/MulterUsers');
+const MulterCompanies = require('./config/MulterCompanies');
+const MulterPoints = require('./config/MulterPoints');
 
 routesAuth.use(authMiddleware);
+routesAuth.use(express.urlencoded({extended: true }));
+routesAuth.use(morgan('dev'));
 
+routesAuth.post('/users/upload',multer(MulterUsers).single('file'),UserController.upload);
 routesAuth.get('/users', UserController.index);
 routesAuth.delete('/users/delete', UserController.delete);
 
+routesAuth.post('/companies/upload',multer(MulterCompanies).single('file'),CompaniesController.upload);
 routesAuth.get('/companies',CompaniesController.index);
 routesAuth.delete('/companies/delete',CompaniesController.delete);
 
+routesAuth.post('/point/upload',multer(MulterPoints).single('file'),PointController.upload);
 routesAuth.get('/point',PointController.index);
 routesAuth.delete('/point/delete',PointController.delete);
 
@@ -22,5 +33,6 @@ routesAuth.post('/discarts/user', DiscartController.userCreate);
 routesAuth.post('/discarts/company', DiscartController.companyCreate);
 routesAuth.post('/discarts/point', DiscartController.pointCreate);
 routesAuth.get('/discarts/points', DiscartController.searchPointForUser);
+
 
 module.exports = routesAuth;
