aa4cfa9605e13caef81a8771c3e1cbb36e9478bd
Kaique Ryan <kaique.github@gmail.com>
2020-05-11 17:23:15 -0300

    Added morgan http log

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes.js');
 const routesAuth = require('./routesAuth');
+const morgan = require('morgan');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(routes);
 app.use(routesAuth);
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.listen(process.env.PORT || 3001);
 
 
