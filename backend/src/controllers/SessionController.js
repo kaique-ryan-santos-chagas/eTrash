37ecb9d134ade460d3d02e59303ba38b7bb24e1e
Kaique Ryan <kaique.github@gmail.com>
2020-05-14 15:13:17 -0300

    straighten code

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -11,18 +11,15 @@ function generateToken(params = {}){
 }
 
 module.exports = {
-	userCreate: async (req, res) => {
-		const {username, passwordInput, localLat, localLon} = req.body;
+	
+    userCreate: async (req, res) => {
+		const {name, passwordInput, localLat, localLon} = req.body;
+        const userIDDB = await connection('users').where('name', name).select('id').first();
 
-		const usernameID = await connection('users').where('name', username).select('id').first();
-
-		const usernameDB = await connection('users').where('name', username)
-		.select('name').first();
-
-		const passwordDB = await connection('users').where('name', username)
+		const passwordDB = await connection('users').where('id', userIDDB.id)
 		.select('password').first();
 		
-		if(!usernameDB){
+		if(!userIDDB){
 			return res.status(400).json({error: 'Usuário não encontrado'});
 		}
 
@@ -32,54 +29,50 @@ module.exports = {
 			return res.status(400).json({error: 'Senha incorreta'});
 		}
 		
-		await connection('users').update({latitude: localLat, longitude: localLon });
+		await connection('users').where('id', userIDDB.id).update({latitude: localLat, longitude: localLon });
 		return res.json({
-        user: username, 
-        token: generateToken({id: usernameID.id})
+            user: name, 
+            token: generateToken({id: userIDDB.id})
         });
+
 	},
 
 	companyCreate: async (req, res) => {
         const {name, passwordInput, localLat, localLon} = req.body;
-        const companieID = await connection('companies').where('name',name).select('id')
+        const companyID = await connection('companies').where('name', name).select('id')
         .first();
 
-        const companieNameDB = await connection('companies').where('name', name)
-        .first();
-
-        if (!companieNameDB) {
+        if (!companyID) {
             return res.status(400).json({error: 'Empresa não encontrada'});
         }
 
-        const companiePasswordDB = await connection('companies').where('name', name)
+        const companyPasswordDB = await connection('companies').where('id', companyID.id)
         .select('password')
         .first();
-        const passwordMatch = await bcrypt.compareSync(passwordInput, companiePasswordDB.password);
+        const passwordMatch = await bcrypt.compareSync(passwordInput, companyPasswordDB.password);
         
         if (!passwordMatch) {
             return res.status(400).json({error: 'Senha Inválida'});
         }
 
-        await connection('companies').update({latitude: localLat, longitude: localLon });
+        await connection('companies').where('id', companyID.id).update({latitude: localLat, longitude: localLon });
         return res.json({
-        companie: name,
-        token: generateToken({id: companieID.id})
+            company: name,
+            token: generateToken({id: companyID.id})
         });
+
 	},
 
 	pointCreate: async (req, res) => {
         const {name, passwordInput, localLat, localLon} = req.body;
-        const pointID = await connection('discarts_points').where('name',name).select('id')
+        const pointID = await connection('discarts_points').where('name', name).select('id')
         .first();
 
-        const pointNameDB = await connection('discarts_points').where('name', name)
-        .first();
-
-        if (!pointNameDB) {
-            return res.status(400).json({error: 'Empresa não encontrada'});
+        if (!pointID) {
+            return res.status(400).json({error: 'Ponto de coleta não encontrado'});
         }
 
-        const pointPasswordDB = await connection('discarts_points').where('name', name)
+        const pointPasswordDB = await connection('discarts_points').where('id', pointID.id)
         .select('password')
         .first();
         const passwordMatch = await bcrypt.compareSync(passwordInput, pointPasswordDB.password);
@@ -88,10 +81,10 @@ module.exports = {
             return res.status(400).json({error: 'Senha Inválida'});
         }
 
-        await connection('discarts_points').update({latitude: localLat, longitude: localLon });
+        await connection('discarts_points').where('id', pointID.id).update({latitude: localLat, longitude: localLon });
         return res.json({
             point: name,
             token: generateToken({id: pointID.id})
         });
 	}
-	}
+};
