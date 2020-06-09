48ca7d418d25b86141e7c5aa06ff60b4b286cf0c
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2020-06-09 12:51:48 -0300

    Routes with authentication

--- backend/src/routesAuth.js
+++ backend/src/routesAuth.js
@@ -30,18 +30,18 @@ routesAuth.get('/companies/schedule', CompaniesController.schedule);
 
 routesAuth.post('/point/upload', multer(MulterPoints).single('file'), PointController.upload);
 routesAuth.get('/point', PointController.index);
-routesAuth.delete('/point/delete', PointController.delete); // Erro aqui no delete, diz algo sobre violar a foreign key ???
+routesAuth.delete('/point/delete', PointController.delete);
 
 routesAuth.put('/discarts/user', DiscartController.userCreate);
 routesAuth.put('/discarts/company', DiscartController.companyCreate);
 routesAuth.put('/discarts/point', DiscartController.pointCreate);
-routesAuth.get('/discarts/points', DiscartController.searchPointForUser);//Testa no seu, o meu diz que não consegue ler a posição 0 no vetor se for nulo então não entra no IF (Linha 78), quando os discarts do usuario são os mesmos.
+routesAuth.get('/discarts/points', DiscartController.searchPointForUser);
 
 routesAuth.get('/profile/user', ProfileController.userProfile);
-routesAuth.post('/profile/user/avatar', multer(MulterUsers).single('file'), ProfileController.updateUserAvatar); //Erro na linha 58 (não foi possivel ler a propiedade originalname que é indefinida)
+routesAuth.post('/profile/user/avatar', multer(MulterUsers).single('file'), ProfileController.updateUserAvatar);
 routesAuth.get('/profile/company', ProfileController.companyProfile);
-routesAuth.post('/profile/company/avatar', multer(MulterCompanies).single('file'), ProfileController.updateCompanyAvatar);//Erro na linha 124 (não foi possivel ler a propiedade originalname que é indefinida)
+routesAuth.post('/profile/company/avatar', multer(MulterCompanies).single('file'), ProfileController.updateCompanyAvatar);
 routesAuth.get('/profile/point', ProfileController.pointProfile);
-routesAuth.post('/profile/point/avatar', multer(MulterPoints).single('file'), ProfileController.updatePointAvatar);//Erro na linha 186 (não foi possivel ler a propiedade originalname que é indefinida)
+routesAuth.post('/profile/point/avatar', multer(MulterPoints).single('file'), ProfileController.updatePointAvatar);
 
 module.exports = routesAuth;
