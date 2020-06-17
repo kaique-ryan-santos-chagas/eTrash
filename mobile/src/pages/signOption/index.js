dc587ea71cbd05b6d8d744ba0a7576a13859f900
Kaique Ryan <kaique.github@gmail.com>
2020-06-17 20:26:27 -0300

    Added center view for center text and earth animation

--- mobile/src/pages/signOption/index.js
+++ mobile/src/pages/signOption/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import LottieView from 'lottie-react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
@@ -17,22 +18,34 @@ const SignOption = () => {
 
 	function changeScreenToSignUp(){
 		navigation.navigate('SignUp');
-	}  
+	} 
+
+	const centralTextStr = 'Descarte seu\nresíduo eletrônico\ne ajude o'; 
 
 	return (
 		<View style={styles.container}> 
+			<View style={styles.central}>
+				<Text style={styles.textCentral}>{centralTextStr}</Text>
+			</View>
+
+			<LottieView 
+				source={require('../../assets/animations/earth.json')} 
+				style={styles.earthAnimation}
+				autoPlay loop
+			/>
+
 			<View style={styles.footer}>		
-				<RectButton style={styles.optionButton} onPress={() => changeScreenToSignIn()}>
-					<View style={styles.buttonIcon}>
-						<FontAwesomeIcon style={styles.icon} icon={ faSignInAlt }/>
+				<RectButton style={styles.signInButton} onPress={() => changeScreenToSignIn()}>
+					<View style={styles.buttonIconIn}>
+						<FontAwesomeIcon style={styles.iconIn} icon={ faSignInAlt }/>
 					</View>
-					<Text style={styles.textButton}>Entrar</Text>
+					<Text style={styles.textButtonIn}>Entrar</Text>
 				</RectButton>
-				<RectButton style={styles.optionButton} onPress={() => changeScreenToSignUp()}>
-					<View style={styles.buttonIcon}>
-						<FontAwesomeIcon style={styles.icon} icon={ faUserPlus }/>
+				<RectButton style={styles.signUpButton} onPress={() => changeScreenToSignUp()}>
+					<View style={styles.buttonIconUp}>
+						<FontAwesomeIcon style={styles.iconUp} icon={ faUserPlus }/>
 					</View>
-					<Text style={styles.textButton}>Nova Conta</Text>
+					<Text style={styles.textButtonUp}>Nova Conta</Text>
 				</RectButton>
 			</View>
 		</View>
@@ -50,22 +63,56 @@ const styles = StyleSheet.create({
 		
 	},
 
-	footer: {
-		marginTop: 400,
+	central: {
+		marginTop: 100,
+		marginBottom: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 20
+
 	},
 
-	optionButton: {
+	textCentral: {
+		color: 'black',
+		fontFamily: 'Roboto-Bold',
+		fontSize: 45,
+	},
+
+	earthAnimation: {
+		right: 0,
+		position: 'absolute',
+		marginRight: 10,
+		width: 50,
+		marginRight: 50,
+		marginTop: 25
+	},
+
+	footer: {
+		marginTop: 100
+	},
+
+	signInButton: {
 		backgroundColor: '#38c172',
+		borderRadius: 10,
+		width: 300,
+		height: 60,
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 3
+	},
+
+	signUpButton: {
+		backgroundColor: '#ffffff',
 		borderRadius: 5,
-		width: 200,
+		width: 300,
 		height: 50,
 		justifyContent: 'center',
 		alignItems: 'center',
 		margin: 3
 	},
 
-	buttonIcon: {
-		backgroundColor: '#2E8B57',
+	buttonIconIn: {
+		backgroundColor: 'transparent',
 		borderRadius: 5,
 		height: 50,
 		width: 50,
@@ -76,14 +123,37 @@ const styles = StyleSheet.create({
 		
 	},
 
-	icon: {
+	buttonIconUp: {
+		backgroundColor: 'transparent',
+		borderRadius: 5,
+		height: 50,
+		width: 50,
+		position: 'absolute',
+		left: 0,
+		justifyContent: 'center',
+		alignItems: 'center'
+		
+	},
+
+	iconIn: {
 		color: '#ffffff',
 		height: 50,
 		width: 50,
 	},
 
-	textButton: {
+	iconUp: {
+		color: '#38c172',
+		height: 50,
+		width: 50,
+	},
+
+	textButtonIn: {
 		color: '#ffffff',
+		fontWeight: 'bold',
+	},
+
+	textButtonUp: {
+		color: '#38c172',
 		fontWeight: 'bold',
 	}
 });
