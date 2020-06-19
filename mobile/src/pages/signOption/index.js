8042febdb018606fd24dd3d9b1b4211128154a87
Kaique Ryan <kaique.github@gmail.com>
2020-06-19 18:42:39 -0300

    Changed sign option screen for added animations in screen

--- mobile/src/pages/signOption/index.js
+++ mobile/src/pages/signOption/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, Animated } from 'react-native';
 
 import LottieView from 'lottie-react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -9,7 +9,52 @@ import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 const SignOption = () => {
-	
+
+	const [logoAnim] = useState(new Animated.ValueXY({x: 0, y: 80}));
+	const [logoOpacity] = useState(new Animated.Value(0)); 
+	const [logoText] = useState(new Animated.ValueXY({x: 0, y: 80 }));
+	const [logoTextOpacity] = useState(new Animated.Value(0));
+	const [showTextCentral] = useState(new Animated.ValueXY({x: 0, y: 80}));
+	const [textCentralOpacity] = useState(new Animated.Value(0));
+	const [buttonAnim] = useState(new Animated.ValueXY({x: 0, y: 60}))
+
+	useEffect(() => {
+		Animated.parallel([
+			Animated.spring(logoAnim.y, {
+				toValue: 0,
+				speed: 1,
+				bounciness: 0
+			}),
+			Animated.timing(logoOpacity, {
+				toValue: 1,
+				duration: 500
+			}),	
+			Animated.spring(logoText.y, {
+				toValue: 0,
+				speed: 1,
+				bounciness: 0
+			}),
+			Animated.timing(logoTextOpacity, {
+				toValue: 1,
+				duration: 1000
+			}),			
+			Animated.spring(showTextCentral.y, {
+				toValue: 0,
+				speed: 1,
+				bounciness: 0
+			}),
+			Animated.timing(textCentralOpacity, {
+				toValue: 1,
+				duration: 500
+			}),
+			Animated.spring(buttonAnim.y, {
+				toValue: 0,
+				speed: 1,
+				bounciness: 0
+			}),
+		]).start();
+	}, []);
+
 	const navigation = useNavigation();
 
 	function changeScreenToSignIn() {
@@ -24,9 +69,42 @@ const SignOption = () => {
 
 	return (
 		<View style={styles.container}> 
-			<View style={styles.central}>
+			
+			<Animated.Image style={[
+				styles.logo,
+				{
+					opacity: logoOpacity,
+					transform: [
+						{ translateY: logoAnim.y }
+					]
+				}
+			]} 
+				source={require('../../assets/pictures/icon.jpg')} 
+			/>
+
+			<Animated.Text style={[
+				styles.logoText,
+				{	
+					opacity: logoTextOpacity,
+					transform: [
+						{ translateY: logoText.y }
+					]
+				}
+
+			]}>Trash</Animated.Text>
+
+			<Animated.View style={[
+				styles.central,
+					{
+						opacity: textCentralOpacity, 
+						transform: [
+							{ translateY: showTextCentral.y }
+						]
+					}
+				]}>
+
 				<Text style={styles.textCentral}>{centralTextStr}</Text>
-			</View>
+			</Animated.View>
 
 			<LottieView 
 				source={require('../../assets/animations/earth.json')} 
@@ -34,7 +112,11 @@ const SignOption = () => {
 				autoPlay loop
 			/>
 
-			<View style={styles.footer}>		
+			<Animated.View style={[
+				styles.footer,
+				{ transform: [ { translateY: buttonAnim.y } ] }
+			]}>
+
 				<RectButton style={styles.signInButton} onPress={() => changeScreenToSignIn()}>
 					<View style={styles.buttonIconIn}>
 						<FontAwesomeIcon style={styles.iconIn} icon={ faSignInAlt }/>
@@ -47,7 +129,7 @@ const SignOption = () => {
 					</View>
 					<Text style={styles.textButtonUp}>Nova Conta</Text>
 				</RectButton>
-			</View>
+			</Animated.View>
 		</View>
 
 	);
@@ -63,12 +145,31 @@ const styles = StyleSheet.create({
 		
 	},
 
+	logo: {
+		width: 100,
+		height: 70,
+		marginTop: 15,
+		marginRight: 120,
+		marginBottom: 10
+	},
+
+	logoText: {
+		fontSize: 30,
+		color: '#38c172',
+		right: 0,
+		top: 0,
+		position: 'absolute',
+		marginTop: 70,
+		marginRight: 100,
+		fontFamily: 'Nerik-normal'
+	},
+
 	central: {
-		marginTop: 100,
+		marginTop: 50,
 		marginBottom: 0,
 		justifyContent: 'center',
 		alignItems: 'center',
-		marginLeft: 20
+		marginLeft: 30
 
 	},
 
@@ -80,21 +181,23 @@ const styles = StyleSheet.create({
 
 	earthAnimation: {
 		right: 0,
-		position: 'absolute',
-		marginRight: 10,
 		width: 50,
+		position: 'absolute',
 		marginRight: 50,
-		marginTop: 25
+		marginTop: 40,
+		left: 110
 	},
 
 	footer: {
-		marginTop: 100
+		marginTop: 100,
+		justifyContent: 'center',
+		alignItems: 'center'
 	},
 
 	signInButton: {
 		backgroundColor: '#38c172',
 		borderRadius: 10,
-		width: 300,
+		width: 250,
 		height: 60,
 		justifyContent: 'center',
 		alignItems: 'center',
@@ -104,11 +207,12 @@ const styles = StyleSheet.create({
 	signUpButton: {
 		backgroundColor: '#ffffff',
 		borderRadius: 5,
-		width: 300,
+		width: 200,
 		height: 50,
 		justifyContent: 'center',
 		alignItems: 'center',
-		margin: 3
+		margin: 3,
+		marginTop: 10
 	},
 
 	buttonIconIn: {
