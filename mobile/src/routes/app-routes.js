49c642aab14a5a2136a83c994066a6136b13ad28
Kaique Ryan <kaique.github@gmail.com>
2020-06-25 17:04:53 -0300

    Added app-routes for routes without authentication in app and auth-routes for routes with authentication in app

---
+++ mobile/src/routes/app-routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Slider from '../pages/slider/index';
+import SignOption from '../pages/signOption/index';
+import ChooseUser from '../pages/chooseUser/index';
+import SignIn from '../pages/signIn/index';
+import SignUp from '../pages/signUp/index';
+
+const AppStack = createStackNavigator();
+
+
+const Routes = () => {
+ 
+	return (
+		<NavigationContainer>
+			<AppStack.Navigator 
+				headerMode="none"
+				screenOptions={{
+					cardStyle: {
+						backgroundColor: '#ffffff'
+					}
+				}}>
+				<AppStack.Screen name="Slider" component={Slider} />
+				<AppStack.Screen name="ChooseUser" component={ChooseUser} /> 
+				<AppStack.Screen name="SignOption" component={SignOption} />
+				<AppStack.Screen name="SignIn" component={SignIn} />
+				<AppStack.Screen name="SignUp" component={SignUp} />	
+			</AppStack.Navigator>
+		</NavigationContainer>
+	);
+
+}
+
+export default Routes;
