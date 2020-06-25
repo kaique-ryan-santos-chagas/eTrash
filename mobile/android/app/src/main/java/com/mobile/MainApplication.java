ec61f5dadba339035efe07212afbbf77dfaba9ec
Kaique Ryan <kaique.github@gmail.com>
2020-06-25 16:57:08 -0300

    Create splash screen

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -11,6 +12,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.airbnb.android.react.lottie.LottiePackage;
+
+
 
 
 public class MainApplication extends Application implements ReactApplication {
