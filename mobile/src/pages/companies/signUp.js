5e38ef663d7908e960c2625952f46271a231bc82
Kaique Ryan <kaique.github@gmail.com>
2020-07-27 17:30:55 -0300

    Create a new file sign up for register new company into app

---
+++ mobile/src/pages/companies/signUp.js
@@ -0,0 +1,320 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, 
+		 Text, 
+		 StyleSheet, 
+		 Image, 
+		 Animated, 
+		 TouchableOpacity,
+		 TextInput,
+		 KeyboardAvoidingView  } from 'react-native';
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft, 
+		 faUserPlus, 
+		 faLock,
+		 faBuilding } from '@fortawesome/free-solid-svg-icons';
+
+import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
+import ipApi from '../../services/ip-api';
+
+const SignUpCompany = () => {
+
+	const navigation = useNavigation();
+
+	const [imageCompanyOpacity] = useState(new Animated.Value(0));
+	const [showCompanyImage] = useState(new Animated.ValueXY({x: 0, y: 80}));
+	const [centralViewAnim] = useState(new Animated.ValueXY({x: 0, y: 80}));
+	const [centralViewOpacity] = useState(new Animated.Value(0));
+	const [cnpj, setCnpj] = useState();
+	const [password, setPassword] = useState();
+	const [country, setCountry] = useState();
+	const [city, setCity] = useState();
+	const [region, setRegion] = useState();
+	const [latitude, setLatitude] = useState();
+	const [longitude, setLongitude] = useState();
+	
+	const input1 = useRef(null);
+	const input2 = useRef(null);
+
+	useEffect(() => {
+		Animated.parallel([
+			Animated.timing(imageCompanyOpacity, {
+				toValue: 1,
+				duration: 700,
+				useNativeDriver: true
+			}),
+			Animated.spring(showCompanyImage.y, {
+				toValue: 0,
+				speed: 1,
+				bounciness: 0,
+				useNativeDriver: true
+			}),
+			Animated.timing(centralViewOpacity, {
+				toValue: 1,
+				duration: 700,
+				useNativeDriver: true
+			}),
+			Animated.spring(centralViewAnim.y, {
+				toValue: 0,
+				speed: 1,
+				bounciness: 0,
+				useNativeDriver: true
+			})
+		]).start();
+	}, []);
+
+	const getLocalization = async () => {
+		const response = await ipApi.get('/json');
+		setCountry(response.data.country);
+		setCity(response.data.city);
+		setRegion(response.data.region);
+		setLatitude(response.data.lat);
+		setLongitude(response.data.lon);
+	}
+
+	return (
+		<KeyboardAvoidingView style={styles.container} behavior="padding">
+			<View style={styles.header}>
+				<TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+					<FontAwesomeIcon style={styles.iconArrow} icon={ faArrowLeft } size={16}/>
+				</TouchableOpacity> 
+				<Text style={styles.title}>Empresa</Text>
+			</View>
+
+			<Animated.Image 
+			style={[styles.companyImg, 
+				{ opacity: imageCompanyOpacity, 
+				  transform: [ { translateY: showCompanyImage.y }] }]} 
+			source={require('../../assets/pictures/company.jpg')}/> 
+			
+			
+			<Animated.View 
+			style={[styles.centralView,   
+				{ opacity: centralViewOpacity, 
+				  transform: [ {translateY: centralViewAnim.y} ] } ]
+				}>
+				<FontAwesomeIcon style={styles.iconTitle} icon={ faUserPlus } size={25} />
+				<Text style={styles.text}>Criar uma conta</Text>
+				<Text style={styles.nameLabel}>CNPJ</Text>
+				<TextInput 
+					style={styles.nameInput}
+					onChangeText={text => setCnpj(text)}
+					value={cnpj} 
+					placeholder="Seu CNPJ numérico"
+					keyboardType="numeric"
+					placeholderTextColor="#ffffff"
+					ref={input1}
+					returnKeyType={'next'}
+					onSubmitEditing={() => input2.current.focus()}
+
+				/>
+				<FontAwesomeIcon style={styles.companyIcon} icon={ faBuilding } size={20} />
+				<Text style={styles.passwordLabel}>Senha</Text>
+				<TextInput
+					style={styles.passwordInput}
+					onChangeText={text => setPassword(text)}
+					value={password}
+					placeholder="Sua senha"
+					placeholderTextColor="#ffffff"
+					ref={input2}
+					secureTextEntry={true}
+					
+				/>
+				<FontAwesomeIcon style={styles.passwordIcon} icon={ faLock } size={20} />
+				<TouchableOpacity style={styles.nextButton} 
+				onPress={() => {				
+					if(cnpj != null && password != null && cnpj != '' && password != ''){
+						getLocalization();
+						navigation.navigate('Avatar', {
+					  		cnpjTextInput: cnpj,
+					  		passwordTextInput: password,
+					  		country: country,
+					  		city: city,
+					  		region: region,
+					  		latitude: latitude,
+					  		longitude: longitude,
+					  		user: 'company'
+					  	});
+					}
+					 
+				}}>
+					<Text style={styles.nextText}>Avançar</Text>
+				</TouchableOpacity>
+			</Animated.View>
+		</KeyboardAvoidingView>
+			
+	);
+} 
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	header: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		top: 0,
+		position: 'absolute',
+		width: 600,
+		height: 90,
+		borderRadius: 10,
+		marginBottom: 20,
+		backgroundColor: 'transparent'
+	},
+	backButton: {
+		width: 40,
+		height: 30,
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginTop: 35,
+		marginLeft: 130,
+		backgroundColor: '#ffffff',
+		padding: 10,
+		borderRadius: 10
+
+	},
+	title: {
+		fontSize: 20,
+		color: 'black',
+		fontFamily: 'Roboto-Bold',
+		marginTop: 20,
+		marginBottom: 7,
+		
+	},
+	iconArrow: {
+		color: '#38c172',
+		fontSize: 100
+	},
+	companyImg: {
+		width: 380,
+		height: 250,
+		top: 0,
+		position: 'absolute',
+		marginTop: 70,
+		borderRadius: 70
+	},
+	centralView: {
+		width: 600,
+		height: 450,
+		marginTop: 390,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 500,
+		backgroundColor: '#38c172'
+	},
+	text: {
+		fontSize: 23,
+		fontFamily: 'Roboto-Bold',
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		color: '#ffffff',
+		marginLeft: 200,
+		marginTop: 35
+	},
+	iconTitle: {
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginLeft: 160,
+		marginTop: 38,
+		color: '#ffffff',
+	},
+	nameLabel: {
+		fontSize: 16,
+		color: '#ffffff',
+		marginTop: 95,
+		fontFamily: 'Roboto-Bold',
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginLeft: 170 
+	},
+	nameInput: {
+		width: 300,
+		height: 50,
+		borderWidth: 2,
+		borderColor: '#ffffff',
+		borderRadius: 25,
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginTop: 125,
+		marginLeft: 150 ,
+		color: '#ffffff',
+		fontFamily: 'Roboto-Bold',
+		paddingLeft: 45
+	},
+	companyIcon: {
+		color: '#ffffff',
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginTop: 140,
+		marginLeft: 165
+	},
+	passwordLabel: {
+		fontSize: 16,
+		color: '#ffffff',
+		marginTop: 190,
+		fontFamily: 'Roboto-Bold',
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginLeft: 170 
+	},
+	passwordInput: {
+		width: 300,
+		height: 50,
+		borderWidth: 2,
+		borderColor: '#ffffff',
+		borderRadius: 25,
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginTop: 220,
+		marginLeft: 150 ,
+		color: '#ffffff',
+		fontFamily: 'Roboto-Bold',
+		paddingLeft: 45,	
+	},
+	passwordIcon: {
+		color: '#ffffff',
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginTop: 235,
+		marginLeft: 165,
+
+	},
+	nextButton: {
+		width: 300,
+		height: 50,
+		borderRadius: 25,
+		top: 0,
+		left: 0,
+		position: 'absolute',
+		marginTop: 290,
+		marginLeft: 150,
+		backgroundColor: '#ffffff',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	nextText: {
+		color: '#38c172',
+		fontFamily: 'Roboto-Bold',
+		fontSize: 20
+	},
+	
+
+});
+
+
+export default SignUpCompany;
+
+
+
