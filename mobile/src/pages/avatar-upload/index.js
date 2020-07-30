86b924a452b1f64d348a9866ea14e310c59b2614
Kaique Ryan <kaique.github@gmail.com>
2020-07-30 17:46:27 -0300

    Storage user id, token, and user type, and text say welcome to user

--- mobile/src/pages/avatar-upload/index.js
+++ mobile/src/pages/avatar-upload/index.js
@@ -1,19 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View,  StyleSheet, Animated } from 'react-native';
+import { View,  StyleSheet, Animated, Text } from 'react-native';
 
 import { useRoute, useNavigation } from '@react-navigation/native';
 import LottieView from 'lottie-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Avatar = () => {
 
 	const navigation = useNavigation();
 	const route = useRoute();
 
+	useEffect( async () => {
+		try {
+		   await AsyncStorage.setItem('@id', route.params.id);
+		   await AsyncStorage.setItem('@token', route.params.token);
+		   await AsyncStorage.setItem('@user', route.params.user);
+
+		}catch(e){
+			console.log(e);
+		}
+
+	}, []);
+
 	//Terminar fluxo de animação
 
 	return(
 		<View style={styles.container}>
 			<LottieView style={styles.readyAnimation} source={require('../../assets/animations/ready.json')} autoPlay loop />
+			<Text style={styles.welcome}>{route.params.welcome}</Text>
 		</View>
 	);
 }
@@ -27,6 +41,12 @@ const styles = StyleSheet.create({
 	},
 	readyAnimation: {
 		width: 100
+	},
+	welcome: {
+		color: 'white',
+		fontFamily: 'Roboto-Bold',
+		fontSize: 20,
+		marginTop: 20
 	}
 });
 
